34f88d493660f0a0e561e7094f70c46a7756220e
Zhenye Zhu <zhenye.zhu@mail.utoronto.ca>
2023-11-04 13:10:19 -0400

    fix canvas height

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -30,12 +30,12 @@ export default function Home() {
             <div className="absolute flex flex-col gap-y-4 lg:flex-row gap-x-0 lg:gap-x-4 w-full h-full py-14 px-4 sm:px-8 sm:pl-[16rem]">
                 <div className="felx shrink-0 lg:flex-1 h-full flex-col min-w-0 p-4 bg-blue-100 rounded-2xl"
                      >
-                    <div className="h-[7%] flex items-center justify-between px-4">
+                    <div className="h-fit flex items-center justify-between px-4">
 
                         <p className="font-bold text-2xl">{username}'s Home</p>
                         <button className="font-medium bg-white rounded-md px-2 py-1.5 shadow hover:ring-blue-200 hover:ring hover:bg-blue-100">Edit Home</button>
                     </div>
-                    <div className="h-[93%] px-4 pb-4 rounded-2xl" ref={canvasContainerRef}><CanvasContainer dimensions={dimensions}/></div>
+                    <div className="h-[calc(100%-3rem)]  flex-1 px-4 pt-2  rounded-2xl" ref={canvasContainerRef}><CanvasContainer dimensions={dimensions}/></div>
 
                 </div>
 
