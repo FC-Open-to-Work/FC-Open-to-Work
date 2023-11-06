57f42a7d1bc28d171f8bde1e20af0b5f514eece4
frank-skyblue <su.kong@mail.utoronto.ca>
2023-11-06 14:50:37 -0500

    Added some demos for bed

--- client/src/api/getLayoutItems.ts
+++ client/src/api/getLayoutItems.ts
@@ -23,6 +23,10 @@ const getCurrentUserBeds = () => {
     // MOCK DATA
     beds.push({ locX: 101, locY: 170, size: Sizes.QUEEN, orientation: Orientations.LEFT }); // bed
 
+    // beds.push({ locX: 300, locY: 700, size: Sizes.SINGLE, orientation: Orientations.UP }); // bed
+    // beds.push({ locX: 500, locY: 700, size: Sizes.TWIN, orientation: Orientations.RIGHT }); // bed
+    // beds.push({ locX: 700, locY: 700, size: Sizes.FULL, orientation: Orientations.DOWN }); // bed
+    // beds.push({ locX: 900, locY: 700, size: Sizes.KING, orientation: Orientations.LEFT }); // bed
     return beds;
 }
 
