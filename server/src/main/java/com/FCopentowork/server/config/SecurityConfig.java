2ce697705518addc960ac8aa92b36d715bbd4f81
frank-skyblue <su.kong@mail.utoronto.ca>
2024-01-16 23:48:54 -0500

    Configuring CORS for production

--- server/src/main/java/com/FCopentowork/server/config/SecurityConfig.java
+++ server/src/main/java/com/FCopentowork/server/config/SecurityConfig.java
@@ -74,7 +74,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         System.out.println("CorsConfigurationSource: permitted origins");
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost", "http://otw-smarthomehub.online", "http://165.22.238.115"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setMaxAge(-1L);
