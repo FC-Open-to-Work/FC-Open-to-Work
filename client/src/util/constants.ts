eb8d14848bb7e2da9e4e6e93c1c0ccd3359d90b3
frank-skyblue <su.kong@mail.utoronto.ca>
2024-01-17 00:44:07 -0500

    Configuring CORS for production

--- client/src/util/constants.ts
+++ client/src/util/constants.ts
@@ -2,10 +2,8 @@ const api_domain = process.env.REACT_APP_BACKEND_API_DOMAIN
     ? process.env.REACT_APP_BACKEND_API_DOMAIN
     : "localhost";
 
-// const api_url =
-//     "http://" + api_domain + ":" + process.env.REACT_APP_BACKEND_API_PORT;
 const api_url =
-    "http://" + api_domain;
+    "http://" + api_domain + ":" + process.env.REACT_APP_BACKEND_API_PORT;
 
 enum Sizes {
     SINGLE,
